f675d28ab0aaef1a3e0c48794691db2bd3ce4caa
Mijanur Rahman Joy <mrmijanjoy@gmail.com>
2024-01-02 00:05:24 +0600

    Update post.php

--- post/post.php
+++ post/post.php
@@ -8,7 +8,7 @@
         <title>Blog Post </title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
         <!-- Core theme CSS -->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
